03ac04888d044b75f6b4a5cc007d2c603df887f6
MSI\robert <robertjdoig@hotmail.co.uk>
2017-01-18 14:00:49 +0000

    Adding the game manager to the project

---
+++ New Unity Project/Assets/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public int[] sector_size = new int[10];
+   
+}
